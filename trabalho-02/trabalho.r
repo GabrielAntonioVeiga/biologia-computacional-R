67b6fface4be36ebe4392192804b6dbfc42971ec
GabrielAntonioVeiga <gabrielveiga54@gmail.com>
2026-04-19 18:04:51 -0300

    altera para dataset para rede neural do Caenorhabditis elegans

--- trabalho-02/trabalho.r
+++ trabalho-02/trabalho.r
@@ -1,20 +1,22 @@
 #' Este script acompanha o "Exercício 2", carregando no RStudio os dados 
 #' indicados na tarefa EaD2.
 
+library(Matrix)
+library(plyr)
 library(dplyr)
+library(igraph)
 library(ggplot2)
 library(stringr)
-library(plyr)
 library(RColorBrewer)
-library(igraph)
-library(RGraphSpace)
 
 # --- Etapa 1: Importação e Limpeza ---
 # Carregando os dados
-mousedata <- read.table("bio-mouse-gene/bio-mouse-gene.edges", header = FALSE)
+mat <- readMM("/home/veiga/Downloads/bio-celegans/bio-celegans.mtx")
+
+edges <- summary(mat)
 
 # Criando o grafo
-network <- graph_from_edgelist(as.matrix(mousedata[,1:2]), directed = FALSE)
+network <- graph_from_edgelist(as.matrix(edges[,1:2]), directed = FALSE)
 
 # Simplificação: Remove loops e arestas múltiplas (Essencial para métricas limpas)
 network <- simplify(network, remove.multiple = TRUE, remove.loops = TRUE)
